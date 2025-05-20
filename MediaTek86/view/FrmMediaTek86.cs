4cb8e5429bec44933a6cb6c68b34c3d460945521
leaso <leaso@LAPTOP-J3FSCCMI>
2025-05-20 13:00:26 +0200

    Ajoutez des fichiers projet.

---
+++ MediaTek86/view/FrmMediaTek86.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MediaTek86.view
+{
+    public partial class FrmMediaTek86 : Form
+    {
+        public FrmMediaTek86()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmMediaTek86_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
