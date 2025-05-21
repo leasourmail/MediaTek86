c327d8cdb75eae1e678c250925022e23518040b0
leaso <leaso@LAPTOP-J3FSCCMI>
2025-05-21 19:12:17 +0200

    Phase 3 : ajout du model, des outils de connexion et de la documentation technique
    - Création du package BddManager avec la classe de connexion à la base de données (singleton)
    
    - Mise en place du package dal avec notamment la classe initiale de gestion de la chaîne de connexion (Access)
    
    - Implémentation des classes métiers dans le package Model (correspondant aux tables de la BDD)
    
    - Génération et correction des commentaires normalisés pour la documentation XML + génération de la doc en HTML
    
    - Ajout de la documentation technique zippée au projet

--- MediaTek86/Program.cs
+++ MediaTek86/Program.cs
@@ -6,6 +6,14 @@ using System.Windows.Forms;
 
 namespace MediaTek86
 {
+    /// <summary>
+    /// Application de gestion du personnel d'une médiathèque
+    /// </summary>
+    internal class NamespaceDoc
+    {
+
+    }
+
     internal static class Program
     {
         /// <summary>
--- MediaTek86/dal/MotifAccess.cs
+++ MediaTek86/dal/MotifAccess.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MediaTek86.model
+namespace MediaTek86.dal
 {
-    public class Admin
+    public class MotifAccess
     {
     }
 }
